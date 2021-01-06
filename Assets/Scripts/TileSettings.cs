3d0f97c0c7d91111cb3772d16f8331e06020425d
Ruben Rådelius <ruben.radelius@gmail.com>
2021-01-06 22:16:20 +0100

    Fixed bug in TileSettings where Material Index didn't use saved index value on Load

--- Assets/Scripts/TileSettings.cs
+++ Assets/Scripts/TileSettings.cs
@@ -4,17 +4,17 @@ public class TileSettings : MonoBehaviour {
     public int MaterialIndex { get; private set; }
     
     public void LoadMaterial(int index) {
-        var material = FindObjectOfType<MaterialManager>().materials[index];
-        ChangeMaterial(material);
+        MaterialIndex = index;
+        ChangeMaterial();
     }
     
     void OnMouseDown() {
-        var mm = FindObjectOfType<MaterialManager>();
-        MaterialIndex = mm.selectedIndex;
-        ChangeMaterial(mm.materials[MaterialIndex]);
+        MaterialIndex = FindObjectOfType<MaterialManager>().selectedIndex;
+        ChangeMaterial();
     }
 
-    void ChangeMaterial(Material material) {
+    void ChangeMaterial() {
+        var material = FindObjectOfType<MaterialManager>().materials[MaterialIndex];
         GetComponent<Renderer>().sharedMaterial = material;
     }
 }
