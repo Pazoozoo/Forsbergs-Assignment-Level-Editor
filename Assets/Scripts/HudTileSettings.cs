250e568a28f8d2ddf50e6df5fd4256eeedbf72aa
Ruben Rådelius <ruben.radelius@gmail.com>
2021-01-06 21:54:21 +0100

    Adds new tiles + materials, Renames HudTileMaterial script to HudTileSettings and adds tile name to settings, Removes old prefabs

--- Assets/Scripts/HudTileSettings.cs
+++ Assets/Scripts/HudTileSettings.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 /// <summary>
-/// This script allows the user to choose material directly
+/// This script allows the user to change settings directly
 /// from the parent object of the HUD_Tile prefab
 /// </summary>
-public class HudTileMaterial : MonoBehaviour {
+
+public class HudTileSettings : MonoBehaviour {
     public int materialIndex;
+    public string tileName;
 
     void Awake() {
         GetComponentInChildren<HudTile>().SetUp(materialIndex);
+        GetComponent<Text>().text = tileName;
     }
 }
